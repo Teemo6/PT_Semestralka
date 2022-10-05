0637065ac08428f153dd5218cb370295ec118ca1
mach-mikulas <emoklaun1@seznam.cz>
2022-10-05 20:12:34 +0200

    Doplneni atributu

--- src/Sklad.java
+++ src/Sklad.java
@@ -1,11 +1,17 @@
 public class Sklad extends AMisto{
     private int pocetKosu;
     private int casPohybKosu;
+    private int casDoplneniTn;
 
-    public Sklad(IntVector2D pozice, int pocetKosu, int casPohybKosu){
+    public Sklad(IntVector2D pozice, int pocetKosu, int casPohybKosu, int casDoplneniTn){
         this.pozice = pozice;
         this.pocetKosu = pocetKosu;
         this.casPohybKosu = casPohybKosu;
+        this.casDoplneniTn = casDoplneniTn;
+    }
+
+    public int getCasDoplneniTn() {
+        return casDoplneniTn;
     }
 
     public int getPocetKosu() {
